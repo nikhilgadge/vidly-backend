ad58c6b2bfa882d65d754bffec3d0b9dc4d9e233
1 <a1@1s-MacBook-Pro.local>
2024-05-14 18:36:40 +0530

    Add actions

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbUrl = process.env.DB_URL || "mongodb://localhost/vidly";
+const dbUrl = `mongodb+srv://nikgadge111:${process.env.MONGO_PASSWORD}@cluster-vidly.g6ymjpz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-vidly/vidly`;
 
 const connect = async () => {
   await mongoose.connect(dbUrl, {
