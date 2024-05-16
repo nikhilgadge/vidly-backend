fd1eaa3467b1eda2e182711d17fddc94468a9877
Nikhil Gadge <nikhilgadge@hotmail.com>
2024-05-15 23:40:54 -0700

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbUrl = `mongodb+srv://nikgadge111:${process.env.MONGO_PASSWORD}@cluster-vidly.g6ymjpz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-vidly/vidly`;
-
+const dbUrl = process.env.MONGO_PASSWORD ? `mongodb+srv://nikgadge111:${process.env.MONGO_PASSWORD}@cluster-vidly.g6ymjpz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-vidly` : "mongodb://localhost/vidly";
 const connect = async () => {
   await mongoose.connect(dbUrl, {
     useUnifiedTopology: true,
