6756fb8d174e858b2db4ae04a05a8acc278e649f
1 <a1@1s-MacBook-Pro.local>
2024-05-18 14:42:29 +0530

    Modify 1

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const dbUrl = process.env.MONGO_PASSWORD ? `mongodb+srv://nikgadge111:${process.env.MONGO_PASSWORD}@cluster-vidly.g6ymjpz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-vidly` : "mongodb://db/vidly";
+const dbUrl = process.env.MONGO_PASSWORD ? `mongodb+srv://nikgadge111:${process.env.MONGO_PASSWORD}@cluster-vidly.vm8hktb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-vidly` : "mongodb://db/vidly";
 const connect = async () => {
   await mongoose.connect(dbUrl, {
     useUnifiedTopology: true,
